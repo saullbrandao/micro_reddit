1411c83e37f1183ebffe49a465e213d2f7d22324
Saull Brandão <saull@outlook.com>
2022-03-31 11:02:27 -0300

    comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
